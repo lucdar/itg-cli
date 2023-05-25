23604817260a37cc59e6e67f3ef57e7c2de570b7
Lucas Clark <lucas.clark@berkeley.edu>
2023-05-24 17:37:44 -0700

    implement robust command parsing

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-import argparse, simfile
+import argparse
+
 
 def main():
-    # get command using argparse
+    ### Commands Format ###
+    # 'command': (help, [(arg1, help1), (arg2, help2), ...])
+    commands = {
+        'add-pack': ('Add a pack from a supplied zip file or link',
+                     [('path', 'path or url to pack')]),
+        'add-song': ('Add a song from a supplied zip file or link',
+                     [('path', 'path or url to song')]),
+        'config': ('Set the current profile',
+                   [('profile', 'profile to use')]),
+        'ping': ('Ping the server', []),
+        'restart': ('(Re)start the game', []),
+        'status': ('Get the current status of the game', []),
+        'songlist': ('Update the current songlist', []),
+        'stop': ('Stop the game', [])
+    }
+
     parser = argparse.ArgumentParser(description='ITG CLI')
-    parser.add_argument('command', help='command to run')
+    subparsers = parser.add_subparsers(dest='command', required=True)
+    for (command, (command_help, args)) in commands.items():
+        subparser = subparsers.add_parser(command, help=command_help)
+        for (arg, arg_help) in args:
+            subparser.add_argument(arg, help=arg_help)
+
     args = parser.parse_args()
 
     # run command based on supplied arguments
     match args.command:
+        case 'add-pack':
+            ...
+        case 'add-song':
+            ...
+        case 'config':
+            ...
         case 'ping':
             print('pong')
             exit(0)
+        case 'restart':
+            ...
+        case 'status':
+            ...
+        case 'songlist':
+            ...
+        case 'stop':
+            ...
+
 
 if __name__ == '__main__':
     main()
