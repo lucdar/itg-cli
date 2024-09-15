d6785d23321a1e46e9665af0bd0c8e69126362f3
Lucas Clark <laclark2020@gmail.com>
2024-09-14 18:04:53 -0700

    simplify and refactor download_file (using pathlib.Path)

--- commands/utils/download_file.py
+++ commands/utils/download_file.py
@@ -3,12 +3,10 @@ import gdown
 import os
 import pyrfc6266
 import tqdm
-from config import settings
-
-DOWNLOADS = settings.downloads
+from pathlib import Path
 
 
-def download_file(url):
+def download_file(url: str, downloads: Path) -> Path:
     """
     Downloads a file from a URL to the downloads folder and returns a path to the downloaded file.
     Processes Google drive links using gdown and attempts to download other files using requests.
@@ -16,39 +14,66 @@ def download_file(url):
     """
     # TODO: handle mega.nz links
     if "drive.google.com" in url or "drive.usercontent.google.com" in url:
-        return gdown.download(
-            url, quiet=False, fuzzy=True, output=os.path.join(DOWNLOADS, "")
+        download_path = gdown.download(
+            url,
+            quiet=False,
+            fuzzy=True,
+            output=os.path.join(downloads, ""),  # Append trailing /
         )
+        return Path(download_path)
     else:  # try using requests
-        r = requests.get(url, allow_redirects=True)
-        if r.status_code != 200:
-            print(f"Error downloading file from {url}")
-            print("Status code:", r.status_code)
-            exit(1)
-        if "Content-Type" not in r.headers.keys():
-            print("No Content-Type header found")
-            exit(1)
-        elif r.headers["Content-Type"] == "application/zip":
-            if "Content-Disposition" not in r.headers.keys():
-                filename = os.path.basename(url) + ".zip"
-            else:
-                # parse filename from Content-Disposition header
-                # Apparently this is nontrivial so the pyrfc6266 library is used
-                filename = pyrfc6266.parse_filename(r.headers["Content-Disposition"])
-            dest = os.path.join(DOWNLOADS, filename)
-            if os.path.exists(dest):
-                os.remove(dest)
-            chunk_size = 128
-            total_size = r.headers.get("content-length", None)
-            if total_size is not None:
-                total_size = int(total_size)
-            pbar = tqdm.tqdm(total=total_size, unit="B", unit_scale=True, desc=filename)
-            with open(dest, "wb") as file:
-                for chunk in r.iter_content(chunk_size=chunk_size):
-                    file.write(chunk)
-                    pbar.update(len(chunk))
-            pbar.close()
-            return dest
-        else:
-            print("Invalid Content-Type:", r.headers["Content-Type"])
-            exit(1)
+        response = requests.get(url, allow_redirects=True)
+        validate_response(response)
+        filename = get_download_filename(response)
+        dest = downloads.joinpath(filename)
+        if dest.exists():
+            os.remove(dest)
+        download_with_progress(response, filename, dest)
+        return dest
+
+
+def validate_response(r: requests.Response) -> None:
+    """
+    Validates a request response.
+    Raises an exception if the status code is not 200, if the request does not have
+    a content header, or if the Content-Type is not in valid_content_types
+    """
+    # TODO: add support for more filetypes (need to change get_download_filename)
+    valid_content_types = ["application/zip"]
+    if r.status_code != 200:  # 200: OK
+        raise Exception(f"Unsuccessful request to {r.url} with status {r.status_code}")
+    if "Content-Type" not in r.headers:
+        raise Exception("No Content-Type header found")
+    if r.headers["Content-Type"] not in valid_content_types:
+        raise Exception("Invalid Content-Type:", r.headers["Content-Type"])
+
+
+def get_download_filename(r: requests.Response) -> Path:
+    """
+    Returns the filename from the response's Content-Disposition header, the url's
+    basename, or defaults to "download.zip"
+    """
+    if "Content-Disposition" in r.headers:
+        return Path(pyrfc6266.parse_filename(r.headers["Content-Disposition"]))
+    name = os.path.basename(r.url)
+    if name != "":
+        return Path(name).with_suffix(".zip")
+    else:
+        return Path("download.zip")
+
+
+def download_with_progress(r: requests.Response, dest: Path) -> None:
+    """
+    Writes the content from a request r to dest
+    Displays a progress bar in stdout.
+    """
+    chunk_size = 128
+    total_size = r.headers.get("content-length", None)
+    if total_size is not None:
+        total_size = int(total_size)
+    pbar = tqdm.tqdm(total=total_size, unit="B", unit_scale=True, desc=dest.name)
+    with open(dest, "wb") as file:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            file.write(chunk)
+            pbar.update(len(chunk))
+    pbar.close()
