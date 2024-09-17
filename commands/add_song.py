0b381738db4df5cd7a755442e362abf33fa71b1b
Lucas Clark <laclark2020@gmail.com>
2024-09-17 14:30:03 -0700

    clean up add_song

--- commands/add_song.py
+++ commands/add_song.py
@@ -1,158 +1,74 @@
-import json
 import os
 import shutil
-import sys
 import simfile
+from pathlib import Path
 from tempfile import TemporaryDirectory
-from simfile.types import Simfile
 from .utils.download_file import download_file
 from .utils.add_utils import (
-    get_charts_string,
-    move_to_temp,
-    find_simfile_dirs,
+    extract,
+    simfile_paths,
     delete_macos_files,
     print_simfile_data,
     prompt_overwrite,
 )
-from config import settings
-
-SINGLES = settings.singles
-CACHE = settings.cache
 
 
-def print_simfile_choices(simfiles: list[Simfile], jsonOutput=False) -> None:
-    total = len(simfiles)
-    if jsonOutput:
-        simfileDict = []
-        for i, sm in enumerate(simfiles):
-            simfile = {
-                "index": i + 1,
-                "title": sm.title,
-                "artist": sm.artist,
-                "charts": get_charts_string(sm, difficulty_labels=True),
-            }
-            simfileDict.append(simfile)
-        print(json.dumps(simfileDict, indent=4))
-    else:
-        for i, sm in enumerate(simfiles):
-            # format chart list output
-            charts = get_charts_string(sm, difficulty_labels=True)
-            charts = str(charts).replace("'", "")
-            indent = len(str(total)) - len(str(i + 1))
-            chartIndent = len(str(total)) + 3
-            print(
-                " " * indent + f"[{i+1}] {sm.title} - {sm.artist}",
-                " " * chartIndent + f"{charts}",
-                sep="\n",
-            )
+def add_song(args, settings):
+    with Path(TemporaryDirectory()) as temp_directory:
+        if args.path.startswith("http"):
+            path = download_file(args.path, settings.downloads)
+        else:
+            path = Path(args.path).absolute()
 
-
-def add_song(args):
-    # download file if URL
-    if args.path.startswith("http"):
-        path = download_file(args.path)
-        # print("path to download:", path)
-    else:
-        path = os.path.abspath(args.path)
-    
-    with TemporaryDirectory() as working_dir:
         if os.path.exists(path) is False:
             raise Exception("Invalid path:", path)
-        move_to_temp(path, working_dir)
+        if not path.is_dir():
+            path = extract(path)
 
-        # identify simfile
-        print("Searching for valid simfiles...")
-        sm_dirs = find_simfile_dirs(working_dir)
-        if len(sm_dirs) == 0:
-            raise Exception("No valid simfiles found")
-        elif len(sm_dirs) == 1:
-            root = sm_dirs[0]
+        # Move files to temporary working directory
+        # TODO: Maybe needs some logic for moving vs. copying?
+        working_path = temp_directory.joinpath(path.name)
+        path.replace(working_path)
+
+        simfile_dirs = set(map(lambda p: p.parent, simfile_paths(working_path)))
+
+        # Ensure only one simfile was supplied
+        if len(simfile_dirs) == 1:
+            simfile_root = simfile_dirs.pop()
+        elif len(simfile_dirs) == 0:
+            raise Exception("No simfiles found.")
         else:
-            # TODO: Exit in this case
-            print("Prompt: Multiple valid simfiles found:")
-            found_simfiles: list[Simfile] = []
-            found_simfile_paths: list[str] = []
-            for sm_dir in sm_dirs:
-                try:
-                    found_simfiles.append(simfile.opendir(sm_dir, strict=False)[0])
-                    found_simfile_paths.append(sm_dir)
-                except Exception as e:
-                    print(f"Error reading simfile in {sm_dir}: {e}")
-                    continue
-            print_simfile_choices(found_simfiles)
-            total = len(found_simfiles)
-            while True:
-                print(f"Please choose a simfile to add [1-{total}]: ", end="")
-                choice = input()
-                if choice.isdigit() and int(choice) < total + 1 and int(choice) > 0:
-                    # print(sm_dirs)
-                    print(f"Chosen dir: {sm_dirs[int(choice) - 1]}")
-                    root = found_simfile_paths[int(choice) - 1]
-                    break
-                else:
-                    print("Invalid choice. Please choose again.")
+            # TODO: Maybe this behavior should be changed? Sticking with it for now because it's simpler.
+            raise Exception(
+                "More than one simfile in supplied link/directory"
+            ).add_note("Supply songs individually or use add-pack instead.")
 
-        print("Moving song to singles folder...")
+        dest = Path(settings.singles).joinpath(simfile_root.name)
 
-        if settings.delete_macos_files:  # delete macos files if enabled
-            delete_macos_files(root)
-
-        # rename folder to zip name if no containing folder
-        if root == working_dir:
-            # TODO: Test if this works. Is importing from archive the only way this can happen?
-            # What about other archive formats? (rar, 7z, etc.)
-            # TODO: Find a better default to use. sm file name instead? Or sm.title?
-            zip_name = os.path.basename(path).replace(".zip", "")
-            if zip_name == "":
-                # use name of .ssc or .sm file if no zip_name
-                for file in os.listdir(root):
-                    if file.endswith(".ssc") or file.endswith(".sm"):
-                        zip_name = file.replace(".ssc", "").replace(".sm", "")
-                        break
-            new_root = os.mkdir(os.path.join(working_dir, zip_name))
-            for item in filter(lambda x: x!=new_root, os.listdir(root)):
-                shutil.move(item, new_root)
-            root = new_root
-
-        # check if song folder already exists in singles folder
-        dest = os.path.join(SINGLES, os.path.basename(root))
-        if os.path.exists(dest):
+        # Overwrite if needed
+        if dest.exists():
             if not args.overwrite:
-                # TODO: output a diff of simfile metadata
-                if settings.delete_macos_files:  # delete macos files if enabled
-                    delete_macos_files(dest)
-
                 print("Prompt: A folder with the same name already exists.")
-                sm_old = simfile.opendir(dest, strict=False)[0]
-                print_simfile_data(sm_old, "Old Simfile")
-                sm_new = simfile.opendir(root, strict=False)[0]
-                print_simfile_data(sm_new, "New Simfile")
-                if not prompt_overwrite("simfile"):
+                if settings.delete_macos_files:
+                    delete_macos_files(dest)
+                    delete_macos_files(simfile_root)
+                old = simfile.opendir(dest, strict=False)[0]
+                new = simfile.opendir(simfile_root, strict=False)[0]
+                print_simfile_data(old, "Old Simfile")
+                print_simfile_data(new, "New Simfile")
+                if prompt_overwrite("simfile"):
                     exit(1)
-
             shutil.rmtree(dest)
-            # Also delete cache entry if chache is supplied in config
-            if CACHE != "":
-                song_folder_name = os.path.basename(root)
-                singles_packname = os.path.basename(os.path.normpath(SINGLES))
-                cache_entry_name = f"Songs_{singles_packname}_{song_folder_name}"
-                cache_entry = os.path.join(CACHE, "Songs", cache_entry_name)
-                if os.path.exists(cache_entry):
-                    os.remove(cache_entry)
-                else:
-                    print("Warning: Cache entry not found. Skipping.")
+            if settings.cache:  # Delete cache entry if it exists
+                cache_entry_name = (
+                    f"Songs_{simfile_root.name}_{Path(settings.singles).name}"
+                )
+                Path(settings.cache).joinpath("Songs", cache_entry_name).unlink(
+                    missing_ok=True
+                )
 
-        # Move the song to the singles folder
-        shutil.move(root, dest)
-    try:
-        sm = simfile.opendir(dest)[0]
-    except Exception as e:
-        if e.__class__.__name__ == "MSDParseError":
-            print(
-                "Warning: Simfile is malformed. Proceeding with unstrict parsing.",
-                file=sys.stderr,
-            )
-            sm = simfile.opendir(dest, strict=False)[0]
-        else:
-            raise e
-    print_simfile_data(sm, "Song added successfully")
+        if settings.delete_macos_files:
+            delete_macos_files(simfile_root)
+        sm = simfile.opendir(dest, strict=False)[0]
+        simfile_root.rename(dest)
+    print(f"Added {sm.title} to {settings.singles.basename}.")
