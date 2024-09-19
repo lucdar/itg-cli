200ba070219c6f4f80af3ea27dc9872f92b89bc0
Lucas Clark <lucas.clark@berkeley.edu>
2024-09-19 13:25:53 -0700

    cleanup censor/uncensor

--- commands/censor.py
+++ commands/censor.py
@@ -1,73 +1,60 @@
 import simfile
-import os
 import shutil
-import sys
-from config import settings
-
-CENSORED_PATH = settings.censored
-SONGS_PATH = settings.packs
-CACHE = settings.cache
+from config import CLISettings
+from pathlib import Path
 
 
-def censor(args):
-    path = os.path.abspath(args.path)
-    if not os.path.exists(path):
-        print(f"Error: {path} does not exist")
-        sys.exit(1)
-
-    # Check that the path is a simfile
+def censor(args, settings: CLISettings):
+    path = Path(args.path).absolute()
+    # Validate supplied path to sm folder
+    if not path.exists():
+        raise Exception(f"Error: {str(path)} does not exist")
+    if not path.is_relative_to(settings.packs):
+        raise Exception(
+            f"Supplied path ({path}) is not a subdirectory of settings.packs ({settings.packs})"
+        )
     try:
-        simfile.opendir(path, strict=False)
+        sm, _ = simfile.opendir(path, strict=False)
     except Exception as e:
-        print(f"Error: {path} is not a valid simfile directory: {e}")
-        sys.exit(1)
+        raise Exception(f"Error: {path} is not a valid simfile directory: {e}")
 
     # Move the simfile to the censored folder under the same pack subdirectory
-    pack_and_song = os.path.relpath(path, start=SONGS_PATH)
-    destination = os.path.join(CENSORED_PATH, pack_and_song)
-    try:
-        shutil.move(path, destination)
-    except Exception as e:
-        print(f"Error: Could not move {path} to {destination}: {e}")
-        sys.exit(1)
+    pack_and_song = path.relative_to(settings.packs)
+    destination = settings.censored.joinpath(pack_and_song)
+    shutil.move(path, destination)
 
     # Remove the song's cache entry if it exists
-    if CACHE != "":
-        packname, songname = os.path.split(pack_and_song)
-        # Cache Files are named Songs_PackName_SongFolderName
-        cache_entry = f"Songs_{packname}_{songname}"
-        cache_entry_path = os.path.join(CACHE, "Songs", cache_entry)
-        if os.path.exists(cache_entry_path):
-            os.remove(cache_entry_path)
-        else:
-            print("Warning: Cache entry not found. Skipping.")
+    cache_entry_name = f"Songs_{path.name}_{path.parent.name}"
+    settings.cache.joinpath("Songs", cache_entry_name).unlink(missing_ok=True)
+
+    print(f"Censored {sm.title} from {path.parent.name}.")
 
 
-def uncensor():
-    # List the files that are currently censored and build a list of choices
-    song_path_fragments = []  # os.path.join(pack_folder, song_folder) list
-    for pack in os.listdir(CENSORED_PATH):
-        print(f"### {pack} ###")
-        pack_path = os.path.join(CENSORED_PATH, pack)
-        for song in os.listdir(pack_path):
-            song_path_fragments.append(os.path.join(pack, song))
-            print(f"[{len(song_path_fragments)}] {song}")
-        print()
+def uncensor(settings: CLISettings):
+    # List the simfile_paths that are currently censored and build a list of choices
+    simfile_paths: list[Path] = []
+    for pack in filter(Path.is_dir, settings.censored.iterdir()):
+        for simfile_path in filter(Path.is_dir, pack.iterdir()):
+            sm, _ = simfile.opendir(simfile_path)
+            simfile_paths.append(simfile_path)
+            print(f"{len(simfile_paths)}. {sm.title} ({pack.name})")
+    if len(simfile_paths) == 0:
+        print("No censored songs")
+        exit()
 
-    print("Prompt: Please select a song to uncensor ", end="")
+    print(f"Select a song to uncensor (1-{len(simfile_paths)}): ", end="")
     while True:
-        choice = input()
-        i = int(choice)
-        if choice.isdigit() and i < len(song_path_fragments) + 1 and i > 0:
-            chosen_song = song_path_fragments[i - 1]
-            print(f"Chosen song: {chosen_song}")
-            song_path = os.path.join(CENSORED_PATH, chosen_song)
-            destination = os.path.join(SONGS_PATH, os.path.dirname(chosen_song))
-            try:
-                shutil.move(song_path, destination)
-            except Exception as e:
-                print(f"Error: Could not move {song_path} to {destination}: {e}")
-                sys.exit(1)
+        user_input = input()
+        choice = int(user_input) if user_input.isdigit() else 0
+        if choice - 1 in range(len(simfile_paths)):
             break
         else:
             print("Invalid choice. Please choose again.")
+
+    chosen_path = simfile_paths[choice - 1]
+    pack_and_song = chosen_path.relative_to(settings.censored)
+    destination = settings.packs.joinpath(pack_and_song)
+    shutil.move(chosen_path, destination)
+
+    sm, _ = simfile.opendir(destination)
+    print(f"Uncensored {sm.title} from {destination.parent.name}.")
